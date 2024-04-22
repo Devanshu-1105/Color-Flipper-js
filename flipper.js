ff502bf141db01827681d7ff0e378cc1926bfc6f
Devanshu janardan garad <158127154+Devanshu-1105@users.noreply.github.com>
2024-04-22 11:46:42 +0000

    js

--- flipper.js
+++ flipper.js
@@ -1,3 +1,31 @@
-const h1 = document.querySelector("h1")
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
 
-h1.style.backgroundColor ="purple"
+
+buttons.forEach((button)=> {
+    button.addEventListener('click', ((e) => {
+        console.log(e.target.id);
+
+        if (e.target.id ==="a") {
+            body.style.backgroundColor = '#a0d2eb';
+            
+        }
+        if (e.target.id ==="b") {
+            body.style.backgroundColor = 'purple';
+            
+        }
+        if (e.target.id ==="c") {
+            body.style.backgroundColor = '#651ee7';
+            
+        }
+        if (e.target.id ==="d") {
+            body.style.backgroundColor = '#8458B3';
+            
+        }
+         if (e.target.id ==="e") {
+            body.style.backgroundColor = '#494d5f';
+            
+        }
+    }))
+})
+ 
